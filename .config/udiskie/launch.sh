07adf6afe2a20c5be96defa981ab1ac4390b2973
mjd119 <mjd119@pitt.edu>
2020-06-16 13:57:54 -0400

    Udiskie now launches from i3 with a script that kills any already running udiskie processes

---
+++ .config/udiskie/launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script add by mjd119 to prevent multiple instances of udiskie and multiple system tray items
+# Based on polybar launch script from Arch Wiki
+# Terminate already running udiskie instances
+killall -q udiskie 
+# Start udiskie (with system tray icon)
+udiskie -t &
+echo "Udiskie launched..."
