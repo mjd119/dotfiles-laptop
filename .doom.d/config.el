c8b4591b1fbfdd79f5a5cf9bc92132e47e2a29ac
mjd119 <mjd119@pitt.edu>
2020-07-21 21:19:34 -0400

    Added doom-emacs configuration files, sudo i3-swallow alias to shell aliases files, reduced picom fading, expanded mpd module max length for title on polybar

---
+++ .doom.d/config.el
@@ -0,0 +1,102 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+;; 
+;; Proper line wrapping (from https://www.guilhermesalome.com/posts/making-emacs-look-good.html) [added by mjd119]
+(global-visual-line-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1B1720" "#964C7B" "#74DFC4" "#FFE261" "#40B4C4" "#EB64B9" "#B4DCE7" "#FFFFFF"])
+ '(custom-safe-themes
+   (quote
+    ("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" default)))
+ '(fci-rule-color "#544863")
+ '(jdee-db-active-breakpoint-face-colors (cons "#222228" "#40B4C4"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#222228" "#74DFC4"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#222228" "#4E415C"))
+ '(objed-cursor-color "#964C7B")
+ '(pdf-view-midnight-colors (cons "#FFFFFF" "#27212E"))
+ '(rustic-ansi-faces
+   ["#27212E" "#964C7B" "#74DFC4" "#FFE261" "#40B4C4" "#EB64B9" "#B4DCE7" "#FFFFFF"])
+ '(vc-annotate-background "#27212E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#74DFC4")
+    (cons 40 "#a2e0a3")
+    (cons 60 "#d0e182")
+    (cons 80 "#FFE261")
+    (cons 100 "#ffd35f")
+    (cons 120 "#ffc55d")
+    (cons 140 "#FFB85B")
+    (cons 160 "#f89c7a")
+    (cons 180 "#f18099")
+    (cons 200 "#EB64B9")
+    (cons 220 "#ce5ca4")
+    (cons 240 "#b2548f")
+    (cons 260 "#964C7B")
+    (cons 280 "#834973")
+    (cons 300 "#72466b")
+    (cons 320 "#604363")
+    (cons 340 "#544863")
+    (cons 360 "#544863")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
